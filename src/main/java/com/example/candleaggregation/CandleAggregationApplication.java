414cf947c722ae6812661034b40a194f9414f894
mostafaalasmar <asmer.mostafa@gmail.com>
2026-04-29 23:45:22 +0300

    Initial commit

---
+++ src/main/java/com/example/candleaggregation/CandleAggregationApplication.java
@@ -0,0 +1,13 @@
+package com.example.candleaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandleAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandleAggregationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/candleaggregation/CandleAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.candleaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandleAggregationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
